aba38bef769ca39684ac2875c5cb211c43ea9677
Koza4enko <koza4enko.lena@gmail.com>
2022-12-12 00:28:29 +0100

    Виконання task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesContainer = document.querySelector("#boxes");
+const inputNumber = document.querySelector('[type="number"]');
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+function createBoxes(amount) {
+  let boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = 10 * i + 30 + "px";
+    box.style.height = 10 * i + 30 + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  return boxes;
+}
+
+createBtn.addEventListener("click", onButtonCreateBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function onButtonCreateBoxes() {
+  const elements = createBoxes(Number(inputNumber.value));
+  boxesContainer.append(...elements);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+  inputNumber.value = "";
+}
